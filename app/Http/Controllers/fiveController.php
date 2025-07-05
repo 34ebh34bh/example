23180bfc1be9919abbd70aa2304e550d67c86e87
Гена Цидармян <68062620+34ebh34bh@users.noreply.github.com>
2025-07-05 16:56:48 +0300

    add controller

---
+++ app/Http/Controllers/fiveController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\models\Task;
+
+class fiveController extends Controller
+{
+    public function index(Request $request) {
+        $query =  Task::query();
+
+        if ($request->has('name')) {
+            $query->where('name', 'like', '%' . $request->input('name') . '%');
+        }
+
+        if ($request->has('price')) {
+            $query->where('price', 'like', '%' . $request->input('price') . '%');
+        }
+
+        $tasks = $query->get();
+        return view('index', compact('tasks'));
+    }
+}
